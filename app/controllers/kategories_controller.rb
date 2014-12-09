08fee8c5a0c564843c39d86ad44315fdcf53469c
Piotr.Dros@rec-global.com <Piotr.Dros@rec-global.com>
2014-12-09 07:24:11 +0100

    Dodanie poprawek zwiazanych obsluga widokow i kontrolerow dla kategorii

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -17,16 +17,32 @@ class KategoriesController < ApplicationController
   end
 
   def edycja
+    @kategoria = Kategorie.find(params[:id])
+    @licznik = Kategorie.count
   end
+  
+  def aktualizuj
+    @kategoria = Kategorie.find(params[:id])
+    @kategorie = Kategorie.new(kategorie_parametry)
+    if @kategoria.update_attributes(kategorie_parametry)
+      redirect_to(:action=>'pokaz', :id =>@kategoria.id)
+    else
+      @licznik = Kategorie.count
+      render('edycja')
+    end
+    
+  end
+  
 
   def usun
     @kategoria = Kategorie.find(params[:id])
     
   end
   
+ 
   def kasuj
-     kategoria = Kategorie.find(params[:id]).destroy
-     redirect_to(:action =>'index')
+    kategoria = Kategorie.find(params[:id]).destroy
+    redirect_to(:action =>'index')
   end
   
   def utworz
@@ -40,7 +56,7 @@ class KategoriesController < ApplicationController
   end
   
   def kategorie_parametry
-    params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna)
+    params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna, :created_at)
   end
   
 end
